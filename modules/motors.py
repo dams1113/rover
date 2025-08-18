57aa958bd61badfd2f25420980de8804cea4dcb2
Damien Baisset <damien5320@gmail.com>
2025-08-19 00:17:14 +0200

    Mise à jour du code

--- modules/motors.py
+++ modules/motors.py
@@ -6,3 +6,15 @@ def recule():
 
 def stop():
     print("[MOTEUR] Stop")
+
+def handle_movement(cmd):
+    print(f"[MOTEUR] Commande reçue : {cmd}")
+
+    if cmd == "AVANCE":
+        avance()
+    elif cmd == "RECULE":
+        recule()
+    elif cmd == "STOP":
+        stop()
+    else:
+        print("[MOTEUR] Commande inconnue")

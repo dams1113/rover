ccfc7bc210e7e2cad6ec13f3e0cc78252bc92916
dams1113 <93426594+dams1113@users.noreply.github.com>
2025-09-02 18:43:28 +0200

    Update git_update.sh

--- git_update.sh
+++ git_update.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 cd /home/rover/rover || exit
-<<<<<<< HEAD
-echo "Mise à jour en cours via Git..."
-=======
-echo " Mise à jour en cours via Git..."
->>>>>>> 63d386e41cdb4bbdaeb2a1e58f980c6746164e85
+echo "📡 Mise à jour en cours via Git..."
 git reset --hard HEAD
 git pull origin main
-

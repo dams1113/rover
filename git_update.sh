0a93a4adcc51534b4039020d02a5cf1be8f8d797
dams1113 <93426594+dams1113@users.noreply.github.com>
2025-09-02 18:47:50 +0200

    Update git_update.sh

--- git_update.sh
+++ git_update.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd /home/rover/rover || exit
 echo "📡 Mise à jour en cours via Git..."
-git reset --hard HEAD
-git pull origin main
 
+# Réinitialise tous les fichiers modifiés
+git reset --hard HEAD
+
+# Supprime les fichiers non suivis (⚠️ attention à ne pas supprimer des fichiers utiles)
+git clean -fd
+
+# Fait le pull
+git pull origin main

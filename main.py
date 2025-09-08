e081091a1c3a6a3791bbb49e9baca79cb7ed5b8e
dams1113 <93426594+dams1113@users.noreply.github.com>
2025-09-08 16:33:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,25 +5,18 @@ from bot import discord_bot
 from modules.gps_reader import start_gps_loop
 from modules.gps_logger import log_loop
 
-
-def start_gps_logger():
-    """Démarre le logger GPS dans un thread"""
-    t = threading.Thread(target=log_loop, args=(30,), daemon=True)  # 30 secondes par défaut
-    t.start()
-    print("[MAIN] GPS Logger lancé (intervalle = 30s)")
-
-
 def main():
-    # Lancer la boucle GPS (lecture série en continu)
+    # Démarrer la boucle GPS
     start_gps_loop()
     print("[MAIN] Boucle GPS démarrée")
 
-    # Lancer le logger GPS (écriture CSV)
-    start_gps_logger()
+    # Démarrer le logger GPS
+    threading.Thread(target=log_loop, args=(30,), daemon=True).start()
+    print("[MAIN] GPS Logger lancé (intervalle = 30s)")
 
-    # Lancer le bot Discord
+    # Démarrer le bot Discord
     asyncio.run(discord_bot.client.start(discord_bot.TOKEN))
-
 
 if __name__ == "__main__":
     main()
+
